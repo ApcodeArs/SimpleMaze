2f92611154f3c7ee6d4d292a93644c9e8c6adc41
Ars <arseniy59@yandex.ru>
2022-08-31 20:24:42 +0300

    improve MazeGenerator: - add variable start position - changed search algorithm of  finish position

--- SimpleMaze/Assets/Scripts/Utils/MazeGenerator.cs
+++ SimpleMaze/Assets/Scripts/Utils/MazeGenerator.cs
@@ -8,27 +8,41 @@ namespace Utils {
         
         private readonly int _width;
         private readonly int _height;
+
+        private List<Vector2Int> _possibleStartPositions;
+        private Vector2Int _startPosition;
         
         public MazeGenerator(int width, int height) {
             _width = width;
             _height = height;
             
             _cells = new MazeCellData[_width, _height];
+
+            InitPossibleStartPositions();
         }
         
         public Maze Generate() {
             CreateEmpty();
             InitBoundaryCells();
 
-            var startPosition = CreateStartPosition();
-            RemoveWallsWithBackTracker(startPosition);
-            var finishPosition = GetFinishPosition(startPosition);
+            _startPosition = GetStartPosition();
+            RemoveWallsWithBackTracker();
+            var finishPosition = GetFinishPosition();
             
-            var maze = new Maze(_cells, startPosition, finishPosition);
+            var maze = new Maze(_cells, _startPosition, finishPosition);
             
             return maze;
         }
 
+        private void InitPossibleStartPositions() {
+            _possibleStartPositions = new List<Vector2Int>() {
+                new Vector2Int(0, 0),
+                new Vector2Int(_width - 2, 0),
+                new Vector2Int(0, _height - 2),
+                new Vector2Int(_width - 2, _height - 2)
+            };
+        }
+        
         private void CreateEmpty() {
             for (var x = 0; x < _width; x++) {
                 for (var y = 0; y < _height; y++) {
@@ -46,16 +60,10 @@ namespace Utils {
                 _cells[_width - 1, y].IsBottomWall = false;
             }
         }
-
-        //todo improve
-        private Vector2Int CreateStartPosition() {
-            var startCell = _cells[0, 0];
-            return new Vector2Int(startCell.X, startCell.Y);
-        }
         
         //https://habr.com/ru/post/445378/
-        private void RemoveWallsWithBackTracker(Vector2Int startPosition) {
-            var current = _cells[startPosition.x, startPosition.y];
+        private void RemoveWallsWithBackTracker() {
+            var current = _cells[_startPosition.x, _startPosition.y];
             current.IsVisited = true;
             current.DistanceFromStart = 0;
 
@@ -97,21 +105,20 @@ namespace Utils {
             }
         }
         
-        //todo check
-        private Vector2Int GetFinishPosition(Vector2Int startPosition) {
-            var furthest = _cells[startPosition.x, startPosition.y];
-
-            for (var x = 0; x < _cells.GetLength(0); x++) {
-                if (_cells[x, _height - 2].DistanceFromStart > furthest.DistanceFromStart) furthest = _cells[x, _height - 2];
-                if (_cells[x, 0].DistanceFromStart > furthest.DistanceFromStart) furthest = _cells[x, 0];
-            }
-
-            for (var y = 0; y < _cells.GetLength(1); y++) {
-                if (_cells[_width - 2, y].DistanceFromStart > furthest.DistanceFromStart) furthest = _cells[_width - 2, y];
-                if (_cells[0, y].DistanceFromStart > furthest.DistanceFromStart) furthest = _cells[0, y];
+        private Vector2Int GetStartPosition() => _possibleStartPositions[Random.Range(0, _possibleStartPositions.Count)];
+        
+        private Vector2Int GetFinishPosition() {
+            var finishCell = _cells[_startPosition.x, _startPosition.y];
+            
+            for (var x = 0; x < _width - 1; x++) {
+                for (var y = 0; y < _height - 1; y++) {
+                    if (_cells[x, y].DistanceFromStart > finishCell.DistanceFromStart) {
+                        finishCell = _cells[x, y];
+                    }
+                }
             }
             
-            return new Vector2Int(furthest.X, furthest.Y);
+            return new Vector2Int(finishCell.X, finishCell.Y);
         }
     }
 }
