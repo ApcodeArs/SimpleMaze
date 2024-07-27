67bcdd8fc82a1f86879c60be35b12e7b2a509d8c
Ars <arseniy59@yandex.ru>
2024-07-27 17:01:48 +0400

    Add hotkey ScreenShot editor tool

--- SimpleMaze/Assets/Scripts/Helpers/Editor/ScreenshotMenuItem.cs
+++ SimpleMaze/Assets/Scripts/Helpers/Editor/ScreenshotMenuItem.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 
 namespace Helpers.Editor {
     public static class ScreenshotMenuItem {
-        [MenuItem("Custom Tools/Take Screenshot")]
+        [MenuItem("Custom Tools/Take Screenshot %#X")]
         public static void TakeScreenshot() {
             ScreenshotTool.TakeScreenshot();
         }
